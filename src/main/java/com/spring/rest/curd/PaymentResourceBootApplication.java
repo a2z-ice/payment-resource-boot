076af5e2ca3cacdbc56511558b701d447428df7b
baassad <assad@batworld.com>
2023-11-12 12:01:19 -0500

    hibernate 5 spring 2.7

---
+++ src/main/java/com/spring/rest/curd/PaymentResourceBootApplication.java
@@ -0,0 +1,15 @@
+package com.spring.rest.curd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class PaymentResourceBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentResourceBootApplication.class, args);
+	}
+
+}
